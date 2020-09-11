2bfb2451d828866338eec274605be33223e891ef
Ярослав Крышень <ya.kryshen@coloris.com.ua>
2020-09-11 21:27:42 +0300

    create image domain

---
+++ src/main/java/com/example/phportfolio/domain/Image.java
@@ -0,0 +1,84 @@
+package com.example.phportfolio.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Image {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String path;
+    private String title;
+    private String description;
+
+    public Image() {
+    }
+
+    public Image(String path, String title, String description) {
+        this.path = path;
+        this.title = title;
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Image image = (Image) o;
+        return id.equals(image.id) &&
+                path.equals(image.path) &&
+                Objects.equals(title, image.title) &&
+                Objects.equals(description, image.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, path, title, description);
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Image{" +
+                "id=" + id +
+                ", path='" + path + '\'' +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
---
+++ src/main/java/com/example/phportfolio/repository/ImageRepository.java
@@ -0,0 +1,7 @@
+package com.example.phportfolio.repository;
+
+import com.example.phportfolio.domain.Image;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ImageRepository extends CrudRepository<Image, Long> {
+}
