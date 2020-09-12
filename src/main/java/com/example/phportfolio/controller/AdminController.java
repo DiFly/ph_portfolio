c63f8cad026e88187261ab55045853f91f656e1d
Ярослав Крышень <ya.kryshen@coloris.com.ua>
2020-09-12 09:35:45 +0300

    add base template for admin page

---
+++ src/main/java/com/example/phportfolio/controller/AdminController.java
@@ -0,0 +1,15 @@
+package com.example.phportfolio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/admin")
+public class AdminController {
+
+    @GetMapping
+    public String getAdminPage() {
+        return "admin";
+    }
+}
--- src/main/java/com/example/phportfolio/controller/HomeController.java
+++ src/main/java/com/example/phportfolio/controller/HomeController.java
@@ -5,8 +5,4 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class HomeController {
 
-//    @RequestMapping(value = "/")
-//    public String index() {
-//        return "index";
-//    }
 }
