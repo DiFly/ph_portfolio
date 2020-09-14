eb907c9f3d4bf08501c3de2673beb10d7d2c0b07
Ярослав Крышень <ya.kryshen@coloris.com.ua>
2020-09-14 10:05:17 +0300

    chagne Iterable to List

--- src/main/java/com/example/phportfolio/repository/ImageRepository.java
+++ src/main/java/com/example/phportfolio/repository/ImageRepository.java
@@ -3,10 +3,11 @@ package com.example.phportfolio.repository;
 import com.example.phportfolio.domain.Image;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ImageRepository extends CrudRepository<Image, Long> {
     Optional<Image> findByPath(String path);
-    Iterable<Image> findAllByTitleContains(String title);
-    Iterable<Image> findAllByDescriptionContains(String description);
+    List<Image> findAllByTitleContains(String title);
+    List<Image> findAllByDescriptionContains(String description);
 }
--- src/main/java/com/example/phportfolio/service/ImageService.java
+++ src/main/java/com/example/phportfolio/service/ImageService.java
@@ -14,6 +14,7 @@ import java.io.IOException;
 import java.net.MalformedURLException;
 import java.nio.file.*;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -57,7 +58,7 @@ public class ImageService {
         return this.repository.findByPath(path);
     }
 
-    Iterable<Image> findAllByTitleContains(String title){
+    List<Image> findAllByTitleContains(String title){
         return this.repository.findAllByTitleContains(title);
     };
 
