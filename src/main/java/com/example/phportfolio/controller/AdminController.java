7de18fd17d845cad497ab6a7b767231ad84e3fec
difly <kryshen.yaroslav@gmail.com>
2020-09-23 21:56:39 +0300

    add simple thymeleaf model

--- src/main/java/com/example/phportfolio/controller/AdminController.java
+++ src/main/java/com/example/phportfolio/controller/AdminController.java
@@ -1,6 +1,7 @@
 package com.example.phportfolio.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,7 +10,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class AdminController {
 
     @GetMapping
-    public String getAdminPage() {
+    public String getAdminPage(Model model) {
+        model.addAttribute("simpleTitle", "Hello, dear user!");
         return "admin";
     }
 }
